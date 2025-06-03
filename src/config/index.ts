339e6799c9265d1df3f8069146e2e33c8781b3b6
Karthik <karthik@Karthiks-MacBook-Pro.local>
2025-06-03 07:09:05 +0530

    Update env vals

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,14 +9,14 @@ const config: AppConfig = {
   port: parseInt(process.env.PORT || '3000', 10),
   nodeEnv: process.env.NODE_ENV || 'development',
   db: {
-    host: process.env.DB_HOST || 'dev-client-name-aurora-srvless-cluster.cluster-c7wwssaa0ugx.eu-west-3.rds.amazonaws.com',
+    host: process.env.DB_HOST || 'dev-client-name-aurora-srvless-instance-0.c7wwssaa0ugx.eu-west-3.rds.amazonaws.com',
     port: parseInt(process.env.DB_PORT || '5432', 10),
     database: process.env.DB_NAME || 'postgresdb',
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'your-srvless-strong-password-here',
   },
   redis: {
-    host: process.env.REDIS_HOST || 'dev-client-name-valkey-srvless-bop3u1.serverless.euw3.cache.amazonaws.com',
+    host: process.env.REDIS_HOST || 'dev-client-name-valkey-srvless-bop3u1.serverless.euw3.cache.amazonaws.com:6379',
     port: parseInt(process.env.REDIS_PORT || '6379', 10),
     password: process.env.REDIS_PASSWORD || 'your-valkey-srvless-auth-token-here',
   },
